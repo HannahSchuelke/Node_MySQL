df8dd513836744158215e3182b5410af3129344a
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-02-26 09:42:33 -0600

    minimal requirements working

--- amazon.sql
+++ amazon.sql
@@ -6,9 +6,33 @@ USE bamazon;
 
 CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NULL,
-  department_name DECIMAL(10,2) NULL,
-  price INTEGER(10) NULL,
-  stock_quantity INTEGER(10)
-  PRIMARY KEY (id)
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR (30) NOT NULL,
+  price INTEGER(10) NOT NULL,
+  stock_quantity INTEGER(10) NOT NULL,
+  PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dior Cherie", "Perfume", 125, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Spider Plant", "Gardening", 15, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ball", "Toys", 10, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chalk", "Toys", 3, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Barbie", "Toys", 20, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Yoga Pant", "Clothing", 40, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bread", "Bakery", 5, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tweezer", "Beauty", 10, 40);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,161 @@
 // -- Declarations
 var mysql = require("mysql");
-// -- Connecting to MySQL
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+require("dotenv").config();
+
+// -- Connecting to MySQL, settings
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
   user: "root",
-  password: "root",
+  password: process.env.password,
   database: "bamazon"
 });
-
+// calling connect method to mySQL
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
+  startApp();
 });
+// function to display products, price, quantity
+// AS = aliases 
+function displayProducts(){
+  connection.query("SELECT product_name AS 'Product', price AS 'Sales Price', stock_quantity AS 'In Stock', department_name AS 'Department' FROM products", 
+  (err, results, fields) => {
+    if(err){
+      throw err;
+    }
+    console.table(results);
+
+    // console.log(results);
+// .map runs a function on ever item on the array and returns a new array
+    const productArray = results.map(itemInArray => itemInArray.Product);
+    // const productArray2 = results.map(product => {
+    //   return product.Product
+    // })
+
+    // equivalent code to .map() 
+    // const productChoiceArray = [];
+
+    // for(let i = 0; i < results.length; i++){
+    //   productChoiceArray.push(results[i].Product);
+    // }
+    // console.log("with .map(): ", productArray);
+    // console.log("without .map(): ", productChoiceArray);
+
+    promptCustomer(productArray);
+
+    
+  })      
+}
+
+function startApp(){
+  displayProducts();    
+}
+
+function promptCustomer(availableItems){
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What product would you like to buy?",
+      choices: availableItems,
+      name: "customerSelection"
+    },
+    {
+      type: "input",
+      message: "How many would you like to buy?",
+      name: "purchaseQuantity",
+      validate: (value) => {
+        //   makes it so you cannot type a word in for quantity
+        return !isNaN(value);
+      }
+    }
+  ]).then(customerSelection => {
+    checkAvailability(customerSelection);
+  });
+}
+
+function checkAvailability(customerData){
+// object destructuring  
+  let { customerSelection, purchaseQuantity } = customerData;
+
+  // equivalent code:
+  // let product = customerData.customerSelection;
+  // let quantity = customerData.purchaseQuantity;
+
+  // console.log("customerSelection: ", customerSelection);
+  // console.log("purchaseQuantity: ", purchaseQuantity);
+
+  // console.log("product: ", product);
+  // console.log("quantity: ", quantity);
+
+  connection.query("SELECT stock_quantity, product_name, price FROM products WHERE product_name=?", [customerSelection], (err, results, fields)=>{
+    if(err){
+      throw err;
+    }
+
+    
+    let stock_quantity = results[0].stock_quantity;
+    let productPrice = results[0].price;
+    let remainingItems = stock_quantity - purchaseQuantity;
+    let salesPrice = productPrice * purchaseQuantity;
+
+
+    if(remainingItems > -1){
+      if(purchaseQuantity > 1){
+        console.log(`You just purchased ${purchaseQuantity} ${customerSelection}s for $${salesPrice}`);
+      }else {
+        console.log(`You just purchased ${purchaseQuantity} ${customerSelection} for $${salesPrice}`);
+      }
+      
+      return updateDatabase(customerSelection, remainingItems, salesPrice);
+    }
+    
+   console.log("Sorry, we have insufficient quantity");
+   return continueShoppingPrompt();
+
+  });
+
+}
+
+function updateDatabase(itemName, itemInventory, totalSales){
+
+  connection.query("UPDATE products SET ? WHERE ?", [
+    {
+      stock_quantity: itemInventory
+    },
+    {
+      product_name: itemName
+    }
+  ], (err, results, fields) => {
+    if(err){
+      throw err;
+    }
+
+    return continueShoppingPrompt();    
+  });
+
+}
+
+async function continueShoppingPrompt(){
+ const continuePrompt = await inquirer.prompt(
+    [
+      {
+        type: "confirm",
+        message: "Would you like to continue shopping?",
+        name: "continue"
+
+      }
+    ]
+  );
+// continuePrompt would be the variable inside .then()
+  if(continuePrompt.continue){
+    return displayProducts();
+  }
+
+  console.log('Thank you for shopping, please come back soon!');
+  connection.end();
+}
+
+
